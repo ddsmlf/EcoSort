7e40def7fe22e872cf2bebf47c173bc2c3f9d9e4
Melissa <100771723+ddsmlf@users.noreply.github.com>
2023-10-12 13:29:46 +0200

    Add files via upload

--- API/config.py
+++ API/config.py
@@ -4,16 +4,27 @@ import os
 def inference(region_number, detections):
     relPath = os.getcwd()
     
-    path = relPath+"\\app\\config\\"
+    path = relPath+"\\config\\"
 
     json_filename = f'{path}{region_number}_config.json'
     try :
         with open(json_filename, 'r') as config_file:
             config_data = json.load(config_file)
     except :
-        json_filename = f'{path}0_config.json'
-        with open(json_filename, 'r') as config_file:
-            config_data = json.load(config_file)
+        try :
+            json_filename = f'{path}0_config.json'
+            with open(json_filename, 'r') as config_file:
+                config_data = json.load(config_file)
+        except :
+            path = relPath+"/config/"
+            json_filename = f'{path}{region_number}_config.json'
+            try :
+                with open(json_filename, 'r') as config_file:
+                    config_data = json.load(config_file)
+            except :
+                json_filename = f'{path}0_config.json'
+                with open(json_filename, 'r') as config_file:
+                    config_data = json.load(config_file)
     matching_results = {}
     if detections in config_data:
         matching_results[detections] = config_data[detections]
@@ -25,7 +36,7 @@ def inference(region_number, detections):
 def label(id):
     path = os.getcwd()
 
-    config_file_path = path+"\\app\\config\\config.json"
+    config_file_path = path+"\\config\\config.json"
     try:
         with open(config_file_path, 'r') as config_file:
             config_data = json.load(config_file)
@@ -34,5 +45,12 @@ def label(id):
             else:
                 print("Chiffre non trouvÃ© dans la configuration")
                 return("carton")
-    except FileNotFoundError:
-        return "Fichier de configuration introuvable"
+    except :
+        config_file_path = path+"/config/config.json"
+        with open(config_file_path, 'r') as config_file:
+            config_data = json.load(config_file)
+            if str(id) in config_data:
+                return config_data[str(id)]
+            else:
+                print("Chiffre non trouvÃ© dans la configuration")
+                return("carton")
